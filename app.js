1177bcd3628a4c60f9d484f3c3fd5960d4d65f4d
pabloguzman20 <pguzman89@uabc.edu.mx>
2021-11-30 02:43:27 -0800

    HTML verbs form specific route was added

--- app.js
+++ app.js
@@ -47,6 +47,38 @@ app
     });
   });
 
+app
+  .route("/articles/:articleTitle")
+  .get((req, res) => {
+    Article.findOne({ title: req.params.articleTitle }, (error, result) => {
+      !result ? res.send("No matches") : res.send(result);
+    });
+  })
+  .put((req, res) => {
+    Article.updateMany(
+      { title: req.params.articleTitle },
+      { title: req.body.title, content: req.body.content },
+      { overwrite: true },
+      (error) => {
+        !error ? res.send("Successfully update") : res.send(error);
+      }
+    );
+  })
+  .patch((req, res) => {
+    Article.updateOne(
+      { title: req.params.articleTitle },
+      { $set: { title: req.body } },
+      (error) => {
+        !error ? res.send("Succesfully updated article.") : res.send(error);
+      }
+    );
+  })
+  .delete((req, res) => {
+    Article.deleteOne({ title: req.params.articleTitle }, (error) => {
+      !error ? res.send("Article was removed succesfully.") : res.send(error);
+    });
+  });
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000.");
 });
