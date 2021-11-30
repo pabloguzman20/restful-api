796f2758e696ad97dfa70f59315f281bbaf127ec
pabloguzman20 <pguzman89@uabc.edu.mx>
2021-11-30 02:10:50 -0800

    The route GET was added

--- app.js
+++ app.js
@@ -14,9 +14,7 @@ app.use(
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27107/wikiDB", {
-  userNewUrlParser: true,
-});
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
 
 const articleSchena = {
   title: String,
@@ -24,6 +22,11 @@ const articleSchena = {
 };
 
 const Article = mongoose.model("Article", articleSchena);
+app.get("/articles", (req, res) => {
+  Article.find({}, (error, result) => {
+    !error ? res.send(result): res.send(error);
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000.");
